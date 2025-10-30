19187c75155759012cb73297a9dc5c7f5a2640b5
Yunusa Victor <victoryunusa17@gmail.com>
2025-10-30 17:39:03 +0100

    Changed Resend from SMTP to API

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,23 +1,10 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST || 'smtp.zeptomail.com',
-  port: Number(process.env.MAIL_PORT) || 587,
-  secure: false, // Use false for port 587 (STARTTLS)
-  requireTLS: true,
-  auth: {
-    user: process.env.MAIL_USERNAME || 'emailapikey',
-    pass:
-      process.env.MAIL_PASSWORD ||
-      'wSsVR611+xDzXaoumWL7L+s5ng4ABgmkERx13lv163aoHq+Tpcc+lBHOBFT2FfcYQDU9EjBB8LMrmh0E0mEM2d4onF0DACiF9mqRe1U4J3x17qnvhDzNX25fmhKKK48MwgVvmWhpFMwr+g==',
-  },
-  connectionTimeout: 10000, // 10 seconds
-  greetingTimeout: 10000,
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 exports.sendMail = async ({ to, subject, text, html }) => {
-  await transporter.sendMail({
-    from: `"${process.env.MAIL_FROM_NAME || 'Truetab'}" <${
+  await resend.emails.send({
+    from: `${process.env.MAIL_FROM_NAME || 'Truetab'} <${
       process.env.MAIL_FROM_ADDRESS || 'info@truetab.co'
     }>`,
     to,
