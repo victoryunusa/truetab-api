1886fa1c3df96b07bb76491fad0c435b26f56290
Yunusa Victor <victoryunusa17@gmail.com>
2025-10-30 17:12:03 +0100

    Fixed: email smtp issue

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -2,14 +2,17 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST || 'smtp.zeptomail.com',
-  port: Number(process.env.MAIL_PORT) || 465,
-  secure: true, // Use true for port 465
+  port: Number(process.env.MAIL_PORT) || 587,
+  secure: false, // Use false for port 587 (STARTTLS)
+  requireTLS: true,
   auth: {
     user: process.env.MAIL_USERNAME || 'emailapikey',
     pass:
       process.env.MAIL_PASSWORD ||
       'wSsVR611+xDzXaoumWL7L+s5ng4ABgmkERx13lv163aoHq+Tpcc+lBHOBFT2FfcYQDU9EjBB8LMrmh0E0mEM2d4onF0DACiF9mqRe1U4J3x17qnvhDzNX25fmhKKK48MwgVvmWhpFMwr+g==',
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
 });
 
 exports.sendMail = async ({ to, subject, text, html }) => {
